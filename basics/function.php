54724bb0a941bb179fdb9a88a7c4f6159e90b189
Jamie Young <jamielove891@hanmail.net>
2020-09-03 23:41:09 +0900

    ADD heading and paragraph get by URI parameter

---
+++ basics/function.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>function</title>
+</head>
+
+<body>
+    <h1>function</h1>
+    <?php
+    $str = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque libero aspernatur voluptatibus
+    
+    
+    veritatis sunt maiores impedit sapiente illo voluptas aut expedita quisquam, ut non dolorum ab optio, magnam excepturi? Quo.';
+    echo $str;
+    ?>
+    <h2>strlen()</h2>
+    <?php
+    echo strlen($str);
+    ?>
+    <h2>nl2br()</h2>
+    <?php
+    echo nl2br($str);
+    ?>
+</body>
+
+</html>
--- basics/index.php
+++ basics/index.php
@@ -1,19 +1,28 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
-    <title>Web</title>
+    <title>WEB</title>
 </head>
+
 <body>
     <h1>WEB</h1>
     <ol>
-        <li><a href="index.php?id=HTML">HTML</a></li>
-        <li><a href="index.php?id=CSS">CSS</a></li>
-        <li><a href="index.php?id=JavaScript">JavaScript</a></li>
+        <li><a href="?id=HTML">HTML</a></li>
+        <li><a href="?id=CSS">CSS</a></li>
+        <li><a href="?id=JavaScript">JavaScript</a></li>
     </ol>
-    <h2>Heading</h2>
+    <h2>
+        <?php
+        echo $_GET['id'];
+        ?>
+    </h2>
     <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi rem tenetur eaque at eos unde eius, quisquam dolorum consequuntur cumque? Fugit ducimus nisi a. Facere laborum sequi eum rem veritatis officiis iste accusantium aperiam reiciendis dolorem ipsa impedit eos provident repudiandae repellat nobis praesentium inventore, harum amet ducimus. Molestiae ex autem quam consequatur aut dolorum nobis placeat vitae quod distinctio quisquam amet exercitationem dolor consectetur voluptatum provident assumenda ipsum fugiat error aliquam, fugit accusamus? Odio ipsam quo modi unde placeat accusamus aliquam quae atque vitae, nesciunt, sequi optio dignissimos odit voluptates quia quaerat eum sint blanditiis. Dolore omnis nemo debitis.
+        <?php
+        echo file_get_contents('data/' . $_GET['id']);
+        ?>
     </p>
 </body>
+
 </html>

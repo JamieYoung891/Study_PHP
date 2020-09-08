19522a1c0dd61bbfba24f47bc78314cd9f3ffc4d
Jamie Young <jamielove891@hanmail.net>
2020-09-08 23:10:13 +0900

    SEPARATE layout and logic with user-defined functions

--- basics/index.php
+++ basics/index.php
@@ -1,38 +1,39 @@
+<?php
+
+function print_title() {
+  if (isset($_GET['id'])) echo $_GET['id'];
+  else echo 'Welcome';
+}
+
+function print_content() {
+  if (isset($_GET['id'])) echo file_get_contents('data/' . $_GET['id']);
+  else echo 'Hello!';
+}
+
+function print_nav() {
+  $list = scandir('data');
+  for ($i = 2; $i < count($list); $i++) {
+    $item = $list[$i];
+    if ($item !== '.' || $item !== '..')
+      echo '<li><a href="./?id=' . $item . '">' . $item . '</a></li>';
+  }
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 
 <head>
-    <meta charset="UTF-8">
-    <title>WEB</title>
+  <meta charset="UTF-8">
+  <title><?php print_title(); ?></title>
 </head>
 
 <body>
-    <h1><a href="./">WEB</a></h1>
-    <ol>
-        <?php
-        $list = scandir('data');
-        for ($i = 0; $i < count($list); $i++) {
-            $key = $list[$i];
-            if ($key === '.' || $key === '..') continue;
-            else
-        ?>
-            <li><a href="./?id=<?= $key ?>"><?= $key ?></a></li>
-        <?php
-        }
-        ?>
-    </ol>
-    <h2>
-        <?php
-        if (isset($_GET['id'])) echo $_GET['id'];
-        else echo 'Welcome';
-        ?>
-    </h2>
-    <p>
-        <?php
-        if (isset($_GET['id'])) echo file_get_contents('data/' . $_GET['id']);
-        else echo 'Hello!';
-        ?>
-    </p>
+  <h1><a href="./">WEB</a></h1>
+  <ol><?php print_nav(); ?></ol>
+  <h2><?php print_title(); ?></h2>
+  <p><?php print_content(); ?></p>
 </body>
 
 </html>

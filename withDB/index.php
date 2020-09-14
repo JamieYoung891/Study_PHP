d3d7c241911df082a62e5849affae64a993697fb
Jamie Young <jamielove891@hanmail.net>
2020-09-14 21:32:44 +0900

    INIT withDB app

---
+++ withDB/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>WEB</title>
+</head>
+
+<body>
+  <header>
+    <h1>WEB</h1>
+  </header>
+  <nav>
+    <ol>
+      <li>HTML</li>
+    </ol>
+  </nav>
+  <main>
+    <h2>Welcome</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda eligendi reprehenderit labore laborum. Exercitationem distinctio quae iusto labore ad tempore fugit repellat velit odio deleniti omnis quis sunt qui cumque tempora, soluta mollitia repellendus perferendis sit. Obcaecati voluptate corporis explicabo dolore cum commodi accusantium nobis eaque. Neque impedit ullam delectus perspiciatis quo sit deserunt esse voluptate itaque, qui eveniet beatae ad amet asperiores voluptas ab molestias voluptatibus, consequuntur nemo hic laboriosam velit quaerat! Vel asperiores alias minus esse enim? Aspernatur molestias dicta perspiciatis consequatur, hic ea quasi doloribus totam nemo, rerum quam quia obcaecati necessitatibus? Error fugiat dolorem itaque inventore!</p>
+  </main>
+</body>
+
+</html>

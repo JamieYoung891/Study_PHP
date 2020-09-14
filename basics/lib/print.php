ed6899b18f682473cdca29f8274b20f82d9288ff
Jamie Young <jamielove891@hanmail.net>
2020-09-14 18:15:44 +0900

    SECURE the app against XSS

--- basics/lib/print.php
+++ basics/lib/print.php
@@ -1,22 +1,20 @@
 <?php
 
 function print_title() {
-  if (isset($_GET['id'])) echo $_GET['id'];
+  if (isset($_GET['id'])) echo htmlspecialchars($_GET['id']);
   else echo 'Welcome';
 }
 
 function print_content() {
-  if (isset($_GET['id'])) echo file_get_contents('data/' . $_GET['id']);
+  if (isset($_GET['id'])) echo htmlspecialchars(file_get_contents('data/' . $_GET['id']));
   else echo 'Hello!';
 }
 
 function print_nav() {
   $list = scandir('data');
   for ($i = 2; $i < count($list); $i++) {
-    $item = $list[$i];
+    $item = htmlspecialchars($list[$i]);
     if ($item !== '.' || $item !== '..')
       echo '<li><a href="./?id=' . $item . '">' . $item . '</a></li>';
   }
 }
-
-?>
